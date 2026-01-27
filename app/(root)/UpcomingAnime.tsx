60bfbea2a94b0d32fb3c3cee98c3a1c97ea1dd04
nizrr <nizarrr.id@gmail.com>
2026-01-27 16:28:57 +0700

    feat: Add UpcomingAnime section to the homepage, update site metadata and branding, and upgrade react-query.

---
+++ app/(root)/UpcomingAnime.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const UpcomingAnime = () => {
+    return <main>UpcomingAnime</main>;
+};
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,19 @@
-import SeasonNowAnime from '@/app/(root)/SeasonNowAnime'
-import TopAnime from '@/app/(root)/TopAnime'
-import { getSeasonNowAnime, getTopAnime } from '@/lib/api'
+import SeasonNowAnime from "@/app/(root)/SeasonNowAnime";
+import TopAnime from "@/app/(root)/TopAnime";
+import { getSeasonNowAnime, getTopAnime } from "@/lib/api";
+import { UpcomingAnime } from "./UpcomingAnime";
 
 export default async function Home() {
-   const topAnime = await getTopAnime()
-   const seasonNowAnime = await getSeasonNowAnime()
-   return (
-      <div className="container mx-auto">
-         <div className="grid grid-cols-1 gap-4">
-            <TopAnime anime={topAnime} />
-            <SeasonNowAnime anime={seasonNowAnime} />
-         </div>
-      </div>
-   )
+    const topAnime = await getTopAnime();
+    const seasonNowAnime = await getSeasonNowAnime();
+    const 
+    return (
+        <main className="container mx-auto">
+            <UpcomingAnime />
+            <div className="grid grid-cols-1 gap-4">
+                <TopAnime anime={topAnime} />
+                <SeasonNowAnime anime={seasonNowAnime} />
+            </div>
+        </main>
+    );
 }
